d6dd221fd02e4629bea09102a75d70f2040e33ec
tyrano <tyranosaurus@nate.com>
2018-03-02 14:57:38 +0900

    1. multuple data variable   - activity   - user
    2. onClick
      - onButtonClick(View v)

--- app/src/main/java/com/tyranotyrano/databindingtest/MainActivity.java
+++ app/src/main/java/com/tyranotyrano/databindingtest/MainActivity.java
@@ -3,6 +3,8 @@ package com.tyranotyrano.databindingtest;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 import com.tyranotyrano.databindingtest.databinding.ActivityMainBinding;
 import com.tyranotyrano.databindingtest.vo.User;
@@ -16,7 +18,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(MainActivity.this, R.layout.activity_main);
 
+        // Activity 변수 할당
+        binding.setActivity(MainActivity.this);
+
+        // User 변수 할당
         User user = new User("최", "영진");
         binding.setUser(user);
+    }
+
+    public void onButtonClick(View v) {
+        Toast.makeText(MainActivity.this, "버튼 클릭", Toast.LENGTH_SHORT).show();
     }
 }
